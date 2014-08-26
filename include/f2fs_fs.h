38678386342999104602de07a7f51f089b8db79c
Jaegeuk Kim <jaegeuk@kernel.org>
2014-08-26 16:28:12 -0700

    f2fs_dentry_hash: avoid casting unsigned char to singed char
    This can hurt when calculating hash value, resulting in false alarm.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- include/f2fs_fs.h
+++ include/f2fs_fs.h
@@ -680,7 +680,7 @@ extern int dev_fill(void *, __u64, size_t);
 extern int dev_read_block(void *, __u64);
 extern int dev_read_blocks(void *, __u64, __u32 );
 
-f2fs_hash_t f2fs_dentry_hash(const char *, int);
+f2fs_hash_t f2fs_dentry_hash(const unsigned char *, int);
 
 extern struct f2fs_configuration config;
 
